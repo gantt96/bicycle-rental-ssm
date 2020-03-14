788d763e1014d33dda348c1d7260880ae0edd8c6
gantt96 <y1094273824@163.com>
2020-03-14 16:18:07 +0800

    Initial commit

---
+++ src/main/java/com/yuanleigen/bicyclerentalssm/BicycleRentalSsmApplication.java
@@ -0,0 +1,13 @@
+package com.yuanleigen.bicyclerentalssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleRentalSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicycleRentalSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuanleigen/bicyclerentalssm/BicycleRentalSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanleigen.bicyclerentalssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleRentalSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
